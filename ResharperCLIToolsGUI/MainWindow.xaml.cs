1f58ba61406529bb85fe53b5d764e29a4b23baa0
Armand <armandbernard1995@gmail.com>
2022-01-20 22:04:33 +0000

    Add file browser

--- ResharperCLIToolsGUI/MainWindow.xaml.cs
+++ ResharperCLIToolsGUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ResharperCLIToolsGUI
 {
@@ -23,6 +12,21 @@ namespace ResharperCLIToolsGUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Filter = "Solution File|*.sln",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() != true) 
+            {
+                return;
+            }
         }
     }
 }
