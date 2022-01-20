46ebaf685541afa9272e3d365806107102abad7f
Armand <armandbernard1995@gmail.com>
2022-01-20 21:36:26 +0000

    Make CommandRunner static

--- ResharperToolsLib/CommandRunner.cs
+++ ResharperToolsLib/CommandRunner.cs
@@ -4,7 +4,7 @@ namespace ResharperToolsLib
 {
     public class CommandRunner
     {
-        public void Run(string command)
+        public static void Run(string command)
         {
             Process.Start("CMD.exe", command);
         }
