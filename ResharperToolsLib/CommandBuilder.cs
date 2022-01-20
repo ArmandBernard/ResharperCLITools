d8cb56fef2ca263efdbcd046e60bc7350ab612b5
Armand <armandbernard1995@gmail.com>
2022-01-20 18:22:46 +0000

    Create a basic Clean command builder

--- ResharperToolsLib/CommandBuilder.cs
+++ ResharperToolsLib/CommandBuilder.cs
@@ -1,8 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace ResharperToolsLib
 {
-    public class Class1
+    public class CommandBuilder
     {
+        public FileInfo Solution { get; }
+
+        public CommandBuilder(FileInfo solution)
+        {
+            Solution = solution;
+        }
+
+        public string Clean(IList<FileInfo> files = null)
+        {
+            var commandBuilder = new StringBuilder();
+
+            commandBuilder.Append($"jb CleanUpCode \"{Solution.FullName}\"");
+
+            if (files == null)
+            {
+                return commandBuilder.ToString();
+            }
+
+            commandBuilder.Append(" --include=\"");
+
+            var joinedPaths = string.Join(";", files.Select(f => f.FullName));
+
+            commandBuilder.Append(joinedPaths);
+
+            commandBuilder.Append("\"");
+
+            return commandBuilder.ToString();
+        }
     }
 }
