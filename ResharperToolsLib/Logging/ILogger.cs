424be7a06d2ada7d8946cf5d9220f95909827500
Armand <armandbernard1995@gmail.com>
2022-01-31 21:33:03 +0000

    Add loggers to project

---
+++ ResharperToolsLib/Logging/ILogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ResharperToolsLib.Logging
+{
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+
+    public interface ILogger
+    {
+        public void Info(string message);
+
+        public void Warn(string message);
+
+        public void Error(string message, Exception exception = null);
+
+        public void Fatal(string message, Exception exception = null);
+    }
+}
---
+++ ResharperToolsLib/Logging/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ResharperToolsLib.Logging
+{
+    public class Logger : ILogger
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine(formatElements(message, LogLevel.Info));
+        }
+
+        public void Warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception exception = null)
+        {
+            Console.Error.WriteLine(formatElements(message, LogLevel.Error, exception));
+        }
+
+        public void Fatal(string message, Exception exception = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string formatElements(string message, LogLevel logLevel, Exception exception = null)
+        {
+            var output = $"[{DateTime.Now:U}]\t[{logLevel}]\t{message}";
+
+            if (exception != null) output += $"\n{exception}";
+
+            return output;
+        }
+    }
+}
