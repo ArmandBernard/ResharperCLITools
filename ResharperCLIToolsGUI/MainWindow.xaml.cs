7095b974dcc63e9d7d1ad6a45ef16f9a07df7b92
Armand <armandbernard1995@gmail.com>
2022-01-31 20:59:24 +0000

    Add commands for the clean buttons

--- ResharperCLIToolsGUI/MainWindow.xaml.cs
+++ ResharperCLIToolsGUI/MainWindow.xaml.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
+
+using ResharperToolsLib;
+using System.Linq;
 
 namespace ResharperCLIToolsGUI
 {
@@ -51,6 +53,56 @@ namespace ResharperCLIToolsGUI
 
             // shouldn't have to do this, but I can't get binding working
             treeView.ItemsSource = ((MainWindowContext)DataContext).TreeItems;
+        }
+
+        private void CleanButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommandBuilder commandBuilder = new CommandBuilder(SavedDirectory);
+
+            var treeItems = ((MainWindowContext)DataContext).TreeItems;
+
+            var checkedPaths = GetFileIf(treeItems, (item) => item.IsChecked).Select(item => item.Path).ToArray();
+
+            var command = commandBuilder.Clean(checkedPaths);            
+
+            CommandRunner.Run(command);
+        }
+
+        private void CleanAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommandBuilder commandBuilder = new CommandBuilder(SavedDirectory);
+
+            var command = commandBuilder.Clean();
+
+            CommandRunner.Run(command);
+        }
+
+        /// <summary>
+        /// Get all files recursively matching a condition
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="filePredicate"></param>
+        /// <returns></returns>
+        private IList<ITreeItem> GetFileIf(IList<ITreeItem> items, Predicate<FileItem> filePredicate)
+        {
+            List<ITreeItem> itemsN = new List<ITreeItem>();
+
+            foreach (var item in items)
+            {
+                if (item is DirectoryItem dirItem)
+                {
+                    itemsN.AddRange(GetFileIf(dirItem.Items, filePredicate));
+                }
+                else if (item is FileItem fItem)
+                {
+                    if (filePredicate(fItem))
+                    {
+                        itemsN.Add(item);
+                    }                    
+                }
+            }
+
+            return itemsN;
         }
     }
 }
