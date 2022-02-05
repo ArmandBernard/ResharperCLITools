9dc79d0f58d4c7cd0d3fcd3f4eb1e08a3c88d414
Armand <armandbernard1995@gmail.com>
2022-02-05 12:14:04 +0000

    Add FileTree Test for importing check data

---
+++ ResharperToolsLibTests/FileTreeTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ResharperToolsLib.Tree;
+
+namespace ResharperToolsLibTests
+{
+    internal class FileTreeTests
+    {
+        private IList<ITreeItem> Tree1 { get; } = new ITreeItem[]
+        {
+            new DirectoryItem("BaseDirectory", ".\\BaseDirectory")
+            {
+                Items = new[]
+                {
+                    new FileItem("File1", ".\\BaseDirectory\\File1")
+                }
+            }
+        };
+
+        private IList<ITreeItem> Tree2 { get; } = new ITreeItem[]
+        {
+            new DirectoryItem("BaseDirectory", ".\\BaseDirectory")
+            {
+                Items = new[]
+                {
+                    new FileItem("File2", ".\\BaseDirectory\\File2")
+                }
+            }
+        };
+
+        [Test]
+        public void MergeTrees_TwoDifferentTrees_ReturnsMerged()
+        {
+            var merged = FileTree.MergeTrees(Tree1, Tree2);
+
+            Assert.IsTrue(merged.Count == 1, "Expected one tree item at root level");
+            Assert.IsAssignableFrom<DirectoryItem>(merged[0]);
+            var dir = (DirectoryItem) merged[0];
+            Assert.IsTrue(dir.Items.Count == 2, "Expected two files inside first directory");
+        }
+    }
+}
