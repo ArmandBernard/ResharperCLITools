5be76bae94789245f0907365837bffcf37a4bf58
Armand <armandbernard1995@gmail.com>
2022-02-05 09:55:29 +0000

    Update Tests

--- ResharperToolsLibTests/CommandBuilderTests.cs
+++ ResharperToolsLibTests/CommandBuilderTests.cs
@@ -7,64 +7,59 @@ namespace ResharperToolsLibTests
 {
     public class CommandBuilderTests
     {
-        private IList<string> Paths;
+        private readonly string Solution = ".\\ResharperCLITools.sln";
 
-        private string Solution;
+        private readonly IList<string> Paths = new List<string>()
+        {
+            "ExampleApp\\OtherClass.cs",
+            "ExampleApp\\Program.cs"
+        };
 
         [SetUp]
         public void Setup()
         {
-            // set up solution mock
-            Solution = ".\\ResharperCLITools.sln";
-
-            // set up some file mocks
-            var path1 = "ExampleApp\\OtherClass.cs";
-
-            var path2 = "ExampleApp\\Program.cs";
-
-            Paths = new[] { path1, path2 };
         }
 
         [Test]
         public void Clean_NoPathsSpecified_ReturnsWithNoFilter()
         {
             // Arrange
-            var validString = $"jb CleanUpCode \"{Solution}\"";
+            var validString = $"CleanUpCode \"{Solution}\"";
             var commandBuilder = new CommandBuilder(Solution);
 
             // Act
             var command = commandBuilder.Clean();
 
             // Assert
-            Assert.AreEqual(command, validString);
+            Assert.AreEqual(validString, command);
         }
 
         [Test]
         public void Clean_OnePath_ReturnsWithValidFilter()
         {
             // Arrange
-            var validString = $"jb CleanUpCode \"{Solution}\" --include=\"{Paths[0]}\"";
+            var validString = $"CleanUpCode \"{Solution}\" --include=\"{Paths[0]}\"";
             var commandBuilder = new CommandBuilder(Solution);
 
             // Act
             var command = commandBuilder.Clean(new[] { Paths[0] });
 
             // Assert
-            Assert.AreEqual(command, validString);
+            Assert.AreEqual(validString, command);
         }
 
         [Test]
         public void Clean_WithMultiplePaths_ReturnsWithValidFilter()
         {
             // Arrange
-            var validString = $"jb CleanUpCode \"{Solution}\" --include=\"{Paths[0]};{Paths[1]}\"";
+            var validString = $"CleanUpCode \"{Solution}\" --include=\"{Paths[0]};{Paths[1]}\"";
             var commandBuilder = new CommandBuilder(Solution);
 
             // Act
             var command = commandBuilder.Clean(Paths);
 
             // Assert
-            Assert.AreEqual(command, validString);
+            Assert.AreEqual(validString, command);
         }
     }
 }
