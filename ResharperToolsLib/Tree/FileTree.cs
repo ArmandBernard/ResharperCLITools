d067ff51918d4ab808c66e8026a5d919dbb0ba96
Armand <armandbernard1995@gmail.com>
2022-02-05 12:15:29 +0000

    Rename MergeTrees to make it clearer what it does

--- ResharperToolsLib/Tree/FileTree.cs
+++ ResharperToolsLib/Tree/FileTree.cs
@@ -34,16 +34,16 @@ namespace ResharperToolsLib.Tree
             return items;
         }
 
-        public static IList<ITreeItem> MergeTrees(IList<ITreeItem> mergeInto, IList<ITreeItem> mergeWith)
+        public static IList<ITreeItem> ImportCheckState(IList<ITreeItem> newTree, IList<ITreeItem> oldTree)
         {
             var merged = new List<ITreeItem>();
 
-            foreach (var item in mergeInto)
+            foreach (var item in newTree)
             {
                 if (item is FileItem fItem)
                 {
                     // look for same file in mergedWith
-                    var mw = (FileItem?)mergeWith.FirstOrDefault(i => i.Path == item.Path);
+                    var mw = (FileItem?)oldTree.FirstOrDefault(i => i.Equals(item));
 
                     // add the item with checked value from mergeWith if found
                     merged.Add(new FileItem(item.Name, item.Path) { IsChecked = mw != null ? mw.IsChecked : false });
@@ -51,10 +51,10 @@ namespace ResharperToolsLib.Tree
                 else if (item is DirectoryItem dItem)
                 {
                     // look for same directory in mergedWith
-                    var mw = (DirectoryItem?)mergeWith.FirstOrDefault(i => i.Path == item.Path);
+                    var mw = (DirectoryItem?)oldTree.FirstOrDefault(i => i.Equals(item));
 
                     // merge items from each directory
-                    merged.Add(new DirectoryItem(dItem.Name, dItem.Path) { Items = MergeTrees(dItem.Items, mw != null ? mw.Items : new List<ITreeItem>()) });
+                    merged.Add(new DirectoryItem(dItem.Name, dItem.Path) { Items = ImportCheckState(dItem.Items, mw != null ? mw.Items : new List<ITreeItem>()) });
                 }
             }
 
